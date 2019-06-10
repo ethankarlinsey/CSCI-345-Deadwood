f14212b96c4ab0bc7bb0154d06b42a2536739377
Ethan Karlinsey <ekarlinsey98@gmail.com>
2019-06-09 22:34:22 -0700

    Updated casting office message

--- AreaView.java
+++ AreaView.java
@@ -100,7 +100,8 @@ public class AreaView {
 					+ "2" + "\t" + "4" + "\t" + "5" + "\n"
 					+ "3" + "\t" + "10" + "\t" + "10" + "\n"
 					+ "4" + "\t" + "18" + "\t" + "15" + "\n"
-					+ "5" + "\t" + "28" + "\t" + "25" + "\n";
+					+ "5" + "\t" + "28" + "\t" + "25" + "\n"
+					+ "6" + "\t" + "40" + "\t" + "25" + "\n";
 			messageBox.setText(message);
 		}
 		else System.out.println("In AreaView - buildNonSetAreas - the area name was wrong");
